62f3d459461345d20086fbe1029ae3ba22d88de2
Gustavo-grecc0 <gustavocgjsitt@gmail.com>
2023-11-04 01:27:53 -0300

    TesteAPI JS Corsheaders

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const apiUrl = 'http://127.0.0.1:8000/adotante/'; // Replace with your API endpoint URL
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ${response.status}');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data); // Print the response to the console
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+});
